48eb746c5d745e46c5e991aafba98076fd6db042
adam-stafford1 <adam.ryanstafford@gmail.com>
2021-01-27 20:25:17 +0000

    Add project files.

---
+++ BenchmarkSampleAPI/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkSampleAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Linq;
+
+namespace BenchmarkSampleAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public void Privacy(string name)
+        {
+            GetLastName(name);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public string GetLastName(string fullName)
+        {
+            var names = fullName.Split(" ");
+            var lastName = names.LastOrDefault();
+            return lastName ?? string.Empty;
+        }  
+    }
+}
---
+++ BenchmarkSampleTests/PrivacyEndpointTest.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkSampleAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BenchmarkSampleTests
+{
+    // By default will show execution time
+    // The memory diagnoser supports the additional measurement of allocations and GC collections which can be extremely helpful when optimising code.
+
+    [MemoryDiagnoser]
+    public class PrivacyEndpointTest
+    {
+        /// <summary>
+        /// Don’t want to include these within the Benchmark method itself since, want 
+        /// to measure the performance and allocations of the Privacy endpoint in isolation.
+        /// </summary>
+        private const string FullName = "Steve J Gordon";
+        private static readonly HomeController controller = new HomeController();
+
+        [Benchmark(Baseline = true)] // TODO don't fully understand what the baseline does
+        public void PrivacyEndpoint()
+        {
+            controller.Privacy(FullName);
+        }
+    }
+}
